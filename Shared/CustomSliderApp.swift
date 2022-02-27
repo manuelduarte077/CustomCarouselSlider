e49d128d0a49b9a01e2e1e078fa6fbad82c55df3
Manuel Duarte <carlosd99m@gmail.com>
2022-02-26 20:35:45 -0600

    Initial Commit

---
+++ Shared/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  Shared
+//
+//  Created by Manuel Duarte on 26/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
